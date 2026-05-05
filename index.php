ddbbcc50b7008f18582439ae83ad3ddbf3d181d1
FlavioGjoni <f.gjoni@yahoo.com>
2026-05-05 10:50:31 +0200

    No commit

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ $body_data_raw = file_get_contents('php://input');
 
 header('Content-Type: application/json');
 echo json_encode([
-    'request_path' => $uri !== null
-        ? parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-        : null,
+    'request_path' => $uri !== null ? parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) : null,
     'request_method' => $_SERVER['REQUEST_METHOD'] ?? null,
     'request_url' => $_SERVER['REQUEST_URI'] ?? null,
     'request_ip' => $_SERVER['REMOTE_ADDR'] ?? null,
