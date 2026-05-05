b63d4cb3217aded258fb3f1e89cb593a4c295746
FlavioGjoni <f.gjoni@yahoo.com>
2026-05-05 10:23:54 +0200

    Fix some error

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $uri = $_SERVER['REQUEST_URI'] ?? null;
 
 header('Content-Type: application/json');
 echo json_encode([
-    'request_path' => $uri === null
+    'request_path' => $uri !== null
         ? parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
         : null,
     'request_method' => $_SERVER['REQUEST_METHOD'] ?? null,
