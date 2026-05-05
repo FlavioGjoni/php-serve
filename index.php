55e9b8b2dbc8a4f6280edb7f5c35b6ed670eb5d3
FlavioGjoni <f.gjoni@yahoo.com>
2026-05-05 10:31:47 +0200

    Added more things

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 $uri = $_SERVER['REQUEST_URI'] ?? null;
+$body_data_raw = file_get_contents('php://input');
 
 header('Content-Type: application/json');
 echo json_encode([
@@ -9,5 +10,8 @@ echo json_encode([
         : null,
     'request_method' => $_SERVER['REQUEST_METHOD'] ?? null,
     'request_url' => $_SERVER['REQUEST_URI'] ?? null,
+    'request_ip' => $_SERVER['REMOTE_ADDR'] ?? null,
     'headers' => getallheaders(),
+    'query_params' => $_GET ?? [],
+    'body_data_raw' => $body_data_raw,
 ]);
