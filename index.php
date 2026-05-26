5dfad6655ddc67bf043574c20060e318355e9d5b
FlavioGjoni <f.gjoni@yahoo.com>
2026-05-26 14:29:15 +0200

    Changed to public folder

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('max_execution_time', 0);
-
-require_once __DIR__ . '/router/router.php';
